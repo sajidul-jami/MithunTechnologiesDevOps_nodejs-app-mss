712c1c963a4775a64a597c2e8c6f6fcc76b31c32
hanumathub <ghanumath16@gmail.com>
2022-08-02 10:54:31 +0530

    Adding SonarIP

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://172.31.29.183:9000/',
+  serverUrl: 'http://54.84.248.192:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
