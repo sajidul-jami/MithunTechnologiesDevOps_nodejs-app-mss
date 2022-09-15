16de3d9ddc315d1bb0e0db29720a9aaf0181c607
Athiq basha technologies <110050019+athiqathiq@users.noreply.github.com>
2022-09-15 22:43:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cfenv = require('cfenv');
 var app  =  express(); 
 var appEnv = cfenv.getAppEnv();
 var url = process.env.url
-app.set('port', (process.env.PORT || 9981))
+app.set('port', (process.env.PORT || 9985))
 app.use(express.static(__dirname + '/images'))
 
 
@@ -19,7 +19,7 @@ res.send("<h2>Welcome to Node JS express app</h2>"+appEnv.url+appEnv.port+port+p
 }).listen(9009);
 console.log(__dirname+"/images/mithunlogo.jpg");
 */
-app.get('/mithuntechnologies', function(request, response) {
+app.get('/Athiq basha', function(request, response) {
     //response.send("<h2><center>Welcome to Node JS app</h2>");
     response.write("<h2><center><u>Node JS  Application </u></center></h2>");
 	
