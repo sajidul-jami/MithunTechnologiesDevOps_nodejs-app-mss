958411348d1042a9ae8d4dc32041dfca7f758e2a
NVVR <61036801+nvvr53@users.noreply.github.com>
2022-11-05 14:13:04 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://54.221.96.237:9000/',
+  serverUrl: 'http://34.201.178.173:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Sample',
