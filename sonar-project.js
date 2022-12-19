2ca73305711f9b040e23d907625e23b4e42839c8
PravinRai <kevin11starc@gmail.com>
2022-12-19 15:58:48 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -8,7 +8,7 @@ sonarqubeScanner({
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
 	    'sonar.login':'squ_c1875149f72d3dbd41ee3e3a231d2372e226f62c',
 	    //'sonar.login': 'admin',
-	    //'sonar.password': 'admin',
+	    //'sonar.password': 'rachiku3',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
